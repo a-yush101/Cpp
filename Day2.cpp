45cdb7956f97333a6029b9caa566238e6d0418be
a-yus101 <codewayush001@gmail.com>
2025-10-14 13:52:51 +0530

    Day 3 commit

--- Day2.cpp
+++ Day2.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
-void inc(){
-    static int x=5;
-    x++;
-    cout<<x<<endl;
-}
+// void inc(){
+//     static int x=5;
+//     x++;
+//     cout<<x<<endl;
+// }
 int main(){
-    inc();
-    inc();
+    // inc();
+    // inc();
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    for(int i = 1; i < n+1; i++){
+        cout<<i<<" ";
+    }
     return 0;
 }
