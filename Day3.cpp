2d138bdf6339166e4d0f5057d9e50f5753576ca1
a-yus101 <codewayush001@gmail.com>
2025-10-14 13:55:44 +0530

    Day 3 commit

--- Day3.cpp
+++ Day3.cpp
@@ -4,7 +4,7 @@ int main(){
     char ch = 'A';
     for(int i = 0; i < 5; i++){
         for(int j = 0; j <= i; j++){
-            cout<<ch;
+            cout<<ch<<" ";
             ch++;
         }
         cout<<endl;
